6c87fe24dcd1878633c767adba57a4a7e7724f6f
Eugeny Burnashov <shamcode@gmail.com>
2019-01-08 19:28:11 +0700

    Force require sham-ui for jest

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const Compiler = require( 'sham-ui-templates' ).Compiler;
 
 const compiler = new Compiler( { asModule: true } );
 
+function transfromCode( code ) {
+    return `require('sham-ui');\n${code}`;
+}
+
 module.exports = {
     process( src, filename, config, transformOptions ) {
         if ( filename.endsWith( '.sht' ) ) {
             try {
                 let code = compiler.compile( filename, src ).toString();
+                code = transfromCode( code );
                 code = jestTransformer.process( code, filename, config, transformOptions );
                 return code;
             } catch ( error ) {

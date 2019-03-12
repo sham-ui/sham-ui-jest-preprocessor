f2ee75713786f7249397fb8e460310211dc397ee
Eugeny Burnashov <shamcode@gmail.com>
2019-03-12 19:35:23 +0700

    Add support for .sfw

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const jestTransformer = require( 'babel-jest' );
 const Compiler = require( 'sham-ui-templates' ).Compiler;
 
-const compiler = new Compiler( { asModule: true } );
-
-function transfromCode( code ) {
-    return `require('sham-ui');\n${code}`;
-}
+const asModuleCompiler = new Compiler( { asModule: true } );
+const singleFileWidgetCompiler = new Compiler( { asModule: false, asSingleFileWidget: true } );
 
 module.exports = {
     process( src, filename, config, transformOptions ) {
-        if ( filename.endsWith( '.sht' ) ) {
+        if ( filename.endsWith( '.sht' ) || filename.endsWith( '.sfw' ) ) {
             try {
+                const compiler = filename.endsWith( '.sfw' ) ?
+                    singleFileWidgetCompiler :
+                    asModuleCompiler;
                 let code = compiler.compile( filename, src ).toString();
-                code = transfromCode( code );
+                code = `require('sham-ui');\n${code}`;
                 code = jestTransformer.process( code, filename, config, transformOptions );
                 return code;
             } catch ( error ) {
